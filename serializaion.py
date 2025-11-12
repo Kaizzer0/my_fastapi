cd20dacbf50b242e0f3885d3bb1b0ead743f9231
Nguyen Tan Duc <ntduc2005@gmail.com>
2025-11-12 23:14:12 +0700

    converting an object into JSON,str,.. to display in respone

---
+++ serializaion.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+    city: str
+    country: str
+    pin : str
+
+class Patient(BaseModel):
+    name: str
+    age: int
+    address: Address
+
+
+address_dict = {'city' : 'HCM' , 'country' : 'VietNam', 'pin' : '90000'}
+address1 = Address(**address_dict)
+
+patient_dict = {'name' : 'Baki', 'age' : 25, "address" : address1}
+patient1 = Patient(**patient_dict)
+
+
+temp = patient1.model_dump(include=['name', 'age'],exclude='address')
+temp2 = patient1.model_dump_json()
+print(temp)
+print(type(temp))
+print(type(temp2))
